41b649226681eab7af12fd4f8f5d8291ba3525a9
Salih <mr.pagashev@gmail.com>
2021-04-17 00:02:03 +0600

    add some changes

--- src/services/category/categoryService.js
+++ src/services/category/categoryService.js
@@ -2,7 +2,7 @@ import { Category } from '../../models';
 
 class CategoryService {
   async getCategoriesByAppId(appId) {
-    return await Category.findAll({ where: { appId } });
+    return await Category.findAll({ where: { appId }, order: [['createdAt', 'DESC']] });
   }
 
   async addCategory(appId, name, img, status) {
